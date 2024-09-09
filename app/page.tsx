6c46a486727e0da29acb69b8ed04fc44628126e3
Abdi Rahman <abdirinoahash@gmail.com>
2024-09-09 16:00:05 +0300

    new public folder add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/Forms/PatientForm";
+import Image from "next/image";
 
 export default function Home() {
-  return(
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click</Button>
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+            alt="Logo"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-end">© 2024 PulseOra</p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
